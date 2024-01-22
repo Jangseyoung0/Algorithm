20cc408867f6649c979a2ace0b97716077630d46
seyoung <107539679+Jangseyoung0@users.noreply.github.com>
2024-01-22 09:25:58 +0900

    [level 2] Title: 주식가격, Time: 73.46 ms, Memory: 17 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -0,0 +1,12 @@
+def solution(prices):
+    answer = []
+    for i in range(len(prices)):
+        second=0
+        for j in range(i+1,len(prices)):
+            second+=1
+            if prices[i]<=prices[j]:
+                continue
+            else:
+                break
+        answer.append(second)
+    return answer
