72c5818c93a03ea9a4900d256b4bd7a603a49887
seyoung <107539679+Jangseyoung0@users.noreply.github.com>
2024-07-17 17:16:04 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 524 ms, Memory: 60888 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.py
@@ -0,0 +1,26 @@
+import sys
+from collections import deque
+n,m,r=map(int,sys.stdin.readline().split())
+graph=[[] for _ in range(n+1)]
+for _ in range(m):
+    u,v=map(int,sys.stdin.readline().split())
+    graph[u].append(v)
+    graph[v].append(u)
+for i in range(1,n+1):
+    graph[i].sort(reverse=True)
+visited=[0]*(n+1)
+cnt=1
+def bfs(r):
+    global cnt
+    visited[r]=cnt
+    q=deque()
+    q.append(r)
+    while q:
+        cnode=q.popleft()
+        for nnode in graph[cnode]:
+            if visited[nnode]==0:
+                cnt+=1
+                visited[nnode]=cnt
+                q.append(nnode)
+bfs(r)
+print(*visited[1:],sep='\n')
