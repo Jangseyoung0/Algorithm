979d0465ad2a5d31be9af63865938f24148ab60c
seyoung <107539679+Jangseyoung0@users.noreply.github.com>
2024-08-03 01:45:06 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,18 +1,21 @@
 def solution(n, lost, reserve):
     answer = 0
+    a=0
+    for i in lost:
+        if i in reserve:
+            a+=1
+            lost[lost.index(i)]=-1
+            reserve[reserve.index(i)]=-1
     lost.sort()
     reserve.sort()
     
-    for i in lost[:]:
-        if i in reserve:
-            reserve.remove(i)
-            lost.remove(i)
-            
     for i in range(len(lost)):
-        if lost[i]-1 in reserve:
-            reserve.remove(lost[i]-1)
-        elif lost[i]+1 in reserve:
-            reserve.remove(lost[i]+1)
-        else:
-            answer+=1
-    return n-answer
+        if lost[i]!=-1:
+            if lost[i]-1 in reserve:
+                a+=1
+                reserve[reserve.index(lost[i]-1)]=-1
+            elif lost[i]+1 in reserve:
+                a+=1
+                reserve[reserve.index(lost[i]+1)]=-1
+    answer=n-(len(lost)-a)
+    return answer
