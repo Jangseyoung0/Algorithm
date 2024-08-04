8742f94e561b1b92d249f1a80cccb9f09533662e
seyoung <107539679+Jangseyoung0@users.noreply.github.com>
2024-08-04 11:19:47 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,14 +1,12 @@
 def solution(clothes):
-    answer=1
-    clothdict={}
-    for item,cate in clothes:
-        if cate not in clothdict:
-            clothdict[cate]=[item]
+    answer = 1
+    dict={}
+    for i in range(len(clothes)):
+        if clothes[i][1] not in dict:
+            dict[clothes[i][1]]=1
         else:
-            clothdict[cate].append(item)
-    for item in clothdict:
-        answer*=(len(clothdict[item])+1)
-    
+            dict[clothes[i][1]]+=1
+    for i in dict:
+        answer*=(dict[i]+1)
     answer-=1
-    
     return answer
