e2a02a98fabc5a2fdb6d41878e4261bd25cfbfca
seyoung <107539679+Jangseyoung0@users.noreply.github.com>
2024-10-07 00:27:52 +0900

    [Bronze IV] Title: Next in line, Time: 168 ms, Memory: 17552 KB -BaekjoonHub

---
+++ 백준/Bronze/6749. Next in line/Next in line.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main { 
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in); 
+    int y,m;
+    y = sc.nextInt();
+    m = sc.nextInt();
+    System.out.println(m+m-y);
+  }
+}
