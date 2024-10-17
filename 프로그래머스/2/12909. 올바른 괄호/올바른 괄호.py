7b1be0540b9d8f70384bc9073683354db8f66bfe
seyoung <107539679+Jangseyoung0@users.noreply.github.com>
2024-10-18 00:22:42 +0900

    [level 2] Title: 올바른 괄호, Time: 13.29 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,17 +1,17 @@
+from collections import Counter
 def solution(s):
     answer = True
-    rc=0
-    if s[0]==")":
-        answer=False       
-    else:
-        for i in s:
-            if i=="(":
-                rc+=1
-            else:
-                rc-=1
-            if rc<0:
-                answer=False
-    
-    if s[-1]=="(" or rc>0:
-        answer=False         
-    return answer
+    if s[0]==')' or s[-1]=='(':
+        return False
+    left=0
+    right=0
+    for i in range(len(s)):
+        if s[i]=='(':
+            left+=1
+        if s[i]==')':
+            right+=1
+        if left<right:
+            return False
+    if left!=right:
+        return False
+    return True
