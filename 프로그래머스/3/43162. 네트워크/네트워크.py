6cf39750fb96e783fcf8fa9b950707b9cf45337f
seyoung <107539679+Jangseyoung0@users.noreply.github.com>
2024-10-17 22:57:57 +0900

    [level 3] Title: 네트워크, Time: 0.71 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,25 +1,27 @@
 from collections import deque
 def solution(n, computers):
     answer = 0
-    graph=[[] for _ in range(n)]
+    arr=[[]for _ in range(n)]
     for i in range(n):
-        for j in range(n):
+        for j in range(i+1,n):
             if computers[i][j]==1:
-                if j!=i:
-                    graph[i].append(j)
-    visited=[0]*n
-    def bfs(i):
-        q=deque([i])    
-        visited[i]=1
+                arr[i].append(j)
+                arr[j].append(i)
+    def bfs(v,visited,arr):
+        q=deque()
+        if v not in visited:
+            visited.append(v)
+            q.append(v)
         while q:
-            c=q.popleft()
-            for n in graph[c]:
-                if visited[n]==0:
-                    visited[n]=1
-                    q.append(n)
-        return 1
+            target=q.popleft()
+            for i in arr[target]:
+                if i not in visited:
+                    q.append(i)
+                    visited.append(i)
+        return visited
+    visited=[]
     for i in range(n):
-        if visited[i]==0:
-            answer+=bfs(i)
-                
+        if i not in visited:
+            visited=bfs(i,visited,arr)
+            answer+=1
     return answer
